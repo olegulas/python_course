4d06541eca59ce8e951bdb8bbc664dae04eb5573
olegulas <oleg_6699@mail.ru>
2023-01-31 09:03:49 +0300

    commit all made tasks

--- 1_lesson/1_1_homework/main.py
+++ 1_lesson/1_1_homework/main.py
@@ -1,3 +1,10 @@
 # 2. Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. Сколько журавликов сделал
 # каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов, а Катя сделала в два
 # раза больше журавликов, чем Петя и Сережа вместе?
+
+s_bird = int(input("Enter the number of cranes: "))
+
+if not s_bird % 2:
+    print(f"Petya made {int(s_bird * 1 / 6)}, Katya made {int(s_bird * 4 / 6)}, Serega made {int(s_bird * 1 / 6)}")
+else:
+    print("Digit the number of cranes must be even")
