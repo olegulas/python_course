30b6f4fb734548af28a5b93314d4a5636fa1a220
olegulas <oleg_6699@mail.ru>
2023-02-14 00:26:44 +0300

    done 5 homework

---
+++ 5_lesson/5_0_homework/main.py
@@ -0,0 +1,12 @@
+# Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью
+# рекурсии.
+def pow_ab(val1, val2):
+    if val2 == 0:
+        return 1
+    return pow_ab(val1, val2 - 1) * val1
+
+
+if __name__ == "__main__":
+    a = int(input("Please, enter digit: "))
+    a_pow = int(input("Please enter the degree of the number: "))
+    print(f"{a}**{a_pow} = {pow_ab(a, a_pow)}")
